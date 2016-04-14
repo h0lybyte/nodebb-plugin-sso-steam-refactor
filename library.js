30714b4b7ad952f76e849d535a27ade3347f5e70
h0ly byte <admin@kbve.com>
2016-04-14 21:24:15 +0000

    Everything is setup but Library.js

--- library.js
+++ library.js
@@ -36,56 +36,120 @@
 	
   
     Steam.getStrategy = function(strategies, callback) {
-    		meta.settings.get('sso-steam', function(err, settings) {
-    			if (!err && settings['key']) {
-    				passport.use(new passportSteam({
-    				  returnURL: module.parent.require('nconf').get('url') + '/auth/steam/callback',
-              realm: module.parent.require('nconf').get('url'),
-              apiKey: settings['key']
-    				}, function(identifier, profile, done) {
-        process.nextTick(function () {
-          // As Steam Passport does't not provide the username, steamid and avatar information, we have to get from Steam API using http get request.
-          var clientApiKey = Steam.apiKey,
-              Steam64Id = identifier.replace('http://steamcommunity.com/openid/id/', ''),
-              apiUrl = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + clientApiKey + '&steamids=' + Steam64Id,
-              player = {};
-          http.get(apiUrl, function(res) {
-            res.on('data', function(chunck){
-              var responseObj = JSON.parse(chunck.toString());
-              player.id = responseObj.response.players[0].steamid;
-              player.username = responseObj.response.players[0].personaname;
-              player.avatar = responseObj.response.players[0].avatarfull;
-              player.profileurl = responseObj.response.players[0].profileurl;
-
-              Steam.login(player.id, player.username, player.avatar, player.profileurl, function(err, user) {
-                if (err) {
-                  return done(err);
-                }
-                done(null, user);
+        		meta.settings.get('sso-steam', function(err, settings) {
+        			if (!err && settings['key']) {
+        			  
+        			  
+        			//  passport.use(new Steam)
+        			  
+        			  
+        				passport.use(new passportSteam({
+        				  returnURL: module.parent.require('nconf').get('url') + '/auth/steam/callback',
+                  realm: module.parent.require('nconf').get('url'),
+                  apiKey: settings['key']
+        				}, function(identifier, profile, done) {
+            process.nextTick(function () {
+              // As Steam Passport does't not provide the username, steamid and avatar information, we have to get from Steam API using http get request.
+              var clientApiKey = settings['key'],
+                  Steam64Id = identifier.replace('http://steamcommunity.com/openid/id/', ''),
+                  apiUrl = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + clientApiKey + '&steamids=' + Steam64Id,
+                  player = {};
+              http.get(apiUrl, function(res) {
+                res.on('data', function(chunck){
+                  var responseObj = JSON.parse(chunck.toString());
+                  player.id = responseObj.response.players[0].steamid;
+                  player.username = responseObj.response.players[0].personaname;
+                  player.profileurl = responseObj.response.players[0].profileurl;
+    
+                  Steam.login(player.id, player.username, player.profileurl, function(err, user) {
+                    if (err) {
+                      return done(err);
+                    }
+                    done(null, user);
+                  });
+    
+                });
+              }).on('error', function(e) {
+                console.log('problem with request: ' + e.message);
               });
-
             });
-          }).on('error', function(e) {
-            console.log('problem with request: ' + e.message);
+          }));
+    
+          strategies.push({
+            name: 'steam',
+            url: '/auth/steam',
+            callbackURL: '/auth/steam/callback',
+            icon: constants.admin.icon,
+            scope: 'user:username'
           });
+          
+          
+        }
+    
+          callback(null, strategies);
         });
-      }));
-
-      strategies.push({
-        name: 'steam',
-        url: '/auth/steam',
-        callbackURL: '/auth/steam/callback',
-        icon: constants.admin.icon,
-        scope: 'user:username'
-      });
-      
-      
-    }
-
-      callback(null, strategies);
-    });
-  };
+      };
   
+      Steam.getAssociation
+  
+  
+  
+      Steam.login = function(steamID, username, profileUrl, callback) {
+          Steam.getUidBySteamID(steamID, function(err, uid) {
+            if(err) {
+              return callback(err);
+            }
+      
+            if (uid !== null) {
+              // Existing User
+              callback(null, {
+                uid: uid
+              });
+            } else {
+              // New User
+              user.create({username: username}, function(err, uid) {
+                if (err !== null) {
+                  callback(err);
+                } else {
+                  // Save steam-specific information to the user
+                  user.setUserField(uid, 'steamid', steamID);
+                  user.setUserField(uid, 'profileurl', profileUrl);
+                  db.setObjectField('steamid:uid', steamID, uid);
+      
+      
+                  // Save their avatar
+                  //user.setUserField(uid, 'uploadedpicture', avatar);
+                  //user.setUserField(uid, 'picture', avatar);
+      
+                  callback(null, {
+                    uid: uid
+                  });
+                }
+              });
+            }
+          });
+        };
+      
+      Steam.getUidBySteamID = function(steamID, callback) {
+        db.getObjectField('steamid:uid', steamID, function(err, uid) {
+          if (err !== null) {
+            return callback(err);
+          }
+          callback(null, uid);
+        });
+      };
+    
+      
+      Steam.addMenuItem = function(custom_header, callback) {
+    		custom_header.authentication.push({
+    			"route": constants.admin.route,
+    			"icon": constants.admin.icon,
+    			"name": constants.name
+    		});
+
+		callback(null, custom_header);
+	};
+
   
   
 	
